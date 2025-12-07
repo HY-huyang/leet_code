c1534a053a136bb7fc809e53f06a76ca6ec6fb60
HY-huyang <155282732+HY-huyang@users.noreply.github.com>
2025-12-07 14:17:16 +0800

    Delete list directory

--- list/include/ListNode.h
+++
@@ -1,5 +0,0 @@
-#include<iostream>
-#include<list>
-#include<vector>
-#include<unordered_map>
-using namespace std;
--- list/src/main.cpp
+++
@@ -1,9 +0,0 @@
-//面试题02.01.移除重复节点
-
-int main() {      
-    vector<int>ve = { 1, 2, 3, 3, 2, 1 };
-    ListNode* ls = to_list(ve);
-    ListNode* res = Solution().removeDuplicateNodes(ls);
-    cout << res;
-    return 0;
-}
