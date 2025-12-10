053f6a2f2a7eeb484ead22e85f301314f01dd2be
HY-huyang <155282732+HY-huyang@users.noreply.github.com>
2025-12-10 19:02:23 +0800

    1

---
+++ __os.h
@@ -0,0 +1,27 @@
+
+ostream& operator<<(ostream& os, const ListNode* node) {
+    const ListNode* current = node;
+    os << "[";
+    while (current != NULL) {
+        os << current->val;
+        if (current->next != NULL) {
+            os << ", ";
+        }
+        current = current->next;
+    }
+    os << "]";
+    return os;
+}
+
+template<typename T>
+ostream& operator << (ostream& os, const vector<T>& num) {
+    os << '[';
+    int n = num.size();
+    for (int i = 0; i < n; i++) {
+        os << num[i];
+        if (i == n - 1)break;
+        os << ',';
+    }
+    os << ']';
+    return os;
+}
