0377abb416533ea6bae58e57051947b74be72bec
HY-huyang <155282732+HY-huyang@users.noreply.github.com>
2025-12-11 17:22:22 +0800

    Update main.cpp

--- _src_Tree/main.cpp
+++ _src_Tree/main.cpp
@@ -5,3 +5,12 @@ int main() {
     Solution().reorderList(ls);
     return 0;
 }
+
+//0102 二叉树的层序遍历
+int main() {
+    vector<optional<int>>vec = { 3,9,20,nullopt,nullopt,15,7 };
+    TreeNode* tree = to_tree(vec);
+    vector<vector<int>>num = Solution().levelOrder(tree);
+    cout << num;
+    return 0;
+}
