be536c1877a642cb9a2c39f3b2d810aa5077924c
HY-huyang <155282732+HY-huyang@users.noreply.github.com>
2025-12-13 21:22:23 +0800

    Update 0141.cpp

--- _src_List/_list_Solution/0141.cpp
+++ _src_List/_list_Solution/0141.cpp
@@ -1,3 +1,4 @@
+// Ë«Ö¸Õë
 class Solution {
 public:
     bool hasCycle(ListNode* head) {
@@ -8,6 +9,20 @@ public:
             quick = quick->next->next;
             slow = slow->next;
             if (slow == quick || quick->next == slow || slow->next == quick)return true;
+        }
+        return false;
+    }
+};
+
+// unordered_set
+class Solution {
+public:
+    bool hasCycle(ListNode* head) {
+        unordered_set<ListNode*>set;
+        while (head) {
+            if (set.count(head))return true;
+            set.insert(head);
+            head = head->next;
         }
         return false;
     }
